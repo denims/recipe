4bdd755773fe86e3f6bc172b0db8b47171938332
Deni Simon <denimallel@gmail.com>
2018-04-20 00:20:06 +0530

    added recipe Service

--- src/main/java/com/deni/test/recipe/controllers/IndexController.java
+++ src/main/java/com/deni/test/recipe/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.deni.test.recipe.controllers;
 
+import com.deni.test.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"","/","/index"})
-    public String getIndex(){
+    public String getIndex(Model model){
+        model.addAllAttributes(recipeService.getRecipes());
         return "index";
     }
 }
--- src/main/java/com/deni/test/recipe/services/RecipeService.java
+++ src/main/java/com/deni/test/recipe/services/RecipeService.java
@@ -3,8 +3,9 @@ package com.deni.test.recipe.services;
 import com.deni.test.recipe.model.Recipe;
 import com.deni.test.recipe.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
-
+import java.util.ArrayList;
 import java.util.List;
+
 @Service
 public class RecipeService {
 
@@ -12,5 +13,13 @@ public class RecipeService {
 
     public RecipeService(RecipeRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
+    }
+
+    public List<Recipe> getRecipes(){
+        List<Recipe> recipes = new ArrayList<>();
+        for(Recipe recipe:recipeRepository.findAll()){
+            recipes.add(recipe);
+        }
+        return recipes;
     }
 }
