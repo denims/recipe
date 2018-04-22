5b00001f06a0fe6ddd5632f9c188d756a2733183
Deni Simon <denimallel@gmail.com>
2018-04-22 23:46:36 +0530

    Made Java version as 8

--- src/main/java/com/deni/test/recipe/model/Category.java
+++ src/main/java/com/deni/test/recipe/model/Category.java
@@ -1,14 +1,19 @@
 package com.deni.test.recipe.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
 @Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String categoryName;
-    @ManyToMany(mappedBy = "categories")
+    @ManyToMany(mappedBy = "categories",fetch = FetchType.EAGER)
     private Set<Recipe> recipes;
 }
