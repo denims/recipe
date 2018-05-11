74eb90ba1b7ae6db86c27a5a65e1cb6f9dcba61c
Deni Simon <denimallel@gmail.com>
2018-05-11 21:18:08 +0530

    Added controller logic for creating new recipe
    Added new logic to handle new recipe in recipe controller

--- src/main/java/com/deni/test/recipe/controllers/RecipeController.java
+++ src/main/java/com/deni/test/recipe/controllers/RecipeController.java
@@ -1,11 +1,10 @@
 package com.deni.test.recipe.controllers;
 
+import com.deni.test.recipe.commands.RecipeCommand;
 import com.deni.test.recipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class RecipeController {
@@ -20,5 +19,19 @@ public class RecipeController {
     public String getRecipe(Model model, @PathVariable Long id){
         model.addAttribute("recipe",recipeService.getRecipe(id));
         return "recipe/show";
+    }
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        RecipeCommand recipeCommand = new RecipeCommand();
+        model.addAttribute("recipe",recipeCommand);
+        return "recipe/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping("/recipe")
+    public String addOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand savedRecipe = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/"+ savedRecipe.getId();
     }
 }
